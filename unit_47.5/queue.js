c02a0998adf2cc8afa2b9929c75bf79b43c44ce9
CanonWiseman <wisemancanon@gmail.com>
2023-06-27 07:37:21 -0500

    added functionality for stacks and queues

--- unit_47.5/queue.js
+++ unit_47.5/queue.js
@@ -20,26 +20,45 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
+    const newNode = new Node(val);
 
+    if(this.size === 0){
+      this.first = newNode;
+      this.last = newNode;
+      this.size += 1;
+      return undefined;
+    }
+
+    this.size += 1;
+    this.last.next = newNode;
+    this.last = newNode;
+    return undefined;
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
+    if(this.size === 0){
+      throw new Error;
+    }
 
+    const returnVal = this.first.val;
+    this.first = this.first.next;
+    this.size -= 1;
+    return returnVal;
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
-
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0 ? true : false;
   }
 }
 
--- unit_47.5/stack.js
+++ unit_47.5/stack.js
@@ -17,29 +17,49 @@ class Stack {
     this.size = 0;
   }
 
-  /** push(val): add new value to end of the stack. Returns undefined. */
+  /** push(val): add new value to top of the stack. Returns undefined. */
 
   push(val) {
+    const newNode = new Node(val);
 
+    if(this.size === 0){
+      this.first = newNode;
+      this.last = newNode;
+      this.size += 1;
+      return undefined;
+    }
+
+    const prevHead = this.first;
+    this.first = newNode;
+    this.first.next = prevHead;
+    this.size += 1;
+    return undefined;
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
+    if(this.size === 0){
+      throw new Error;
+    }
 
+    const returnVal = this.first.val;
+    this.first = this.first.next;
+    this.size -= 1;
+    return returnVal;
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
-
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0 ? true : false;
   }
 }
 
